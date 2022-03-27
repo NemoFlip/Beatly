ff0e9b03084d2d657e535ebd50b420dbb7aad08b
NemoFlip <artgamer3163@gmail.com>
2022-03-27 09:58:59 +0300

    Initial Commit

---
+++ Beatly/BeatlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatlyApp.swift
+//  Beatly
+//
+//  Created by Артем Хлопцев on 27.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct BeatlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
